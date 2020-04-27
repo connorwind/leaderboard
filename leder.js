66c2c37ce8ab67033dfa54e8cddd7b84e1a1231d
connorwind <60148037+connorwind@users.noreply.github.com>
2020-04-27 16:31:34 -0400

    Add files via upload

--- leder.js
+++ leder.js
@@ -46,7 +46,7 @@ app.get('/get', function(req,res){
 app.post('/post', function(req,res){
 var supename = req.query.name
 var cly = new MongoClient(uri, {useUnifiedTopology: true}, {useCreateIndex: true} );
-client.connect(err=>{
+cly.connect(err=>{
   const collection = client.db("superhero").collection("leaderboard");
   
     collection.updateOne({name:supename, wins:{$gte:1}}, {$inc : {wins:1}}, {upsert:true})
