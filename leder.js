3ab05ce8a51e0c80f7e21a5b14637efded9fef00
connorwind <60148037+connorwind@users.noreply.github.com>
2020-04-27 18:02:13 -0400

    Add files via upload

--- leder.js
+++ leder.js
@@ -15,17 +15,41 @@ app.use(express.static('client'));
   client.connect(err=>{
     const collection = client.db("superhero").collection("leaderboard");
     
-      var obj = collection.find({}).toArray(function(err, result) {
-          if (err) throw err;
-          results = result;
-          console.log(results);
-          //client.close();
-      });
-
+      
 
       app.get('/get', function(req,res){
- 
-        res.send(results)
+        
+    
+    
+  
+        var css = "<html> <head><style> body{background-image: linear-gradient(45deg, #8895ac 0%, #FFFFFF 100%);background-attachment:fixed;height:100%;} h1{padding-left: 2%;text-align:center;padding-top: 50px;padding-right:2%; padding-bottom: 0 !important;position: static; z-index: 100;  color: #363CD2;}.leaderboard{text-align: center;border: 1px solid black;  position:relative;  margin-left:auto;margin-right:auto;color: #363CD2;} </style></head>"
+        var html = "<body><h1>LEADERBOARD</h1>"
+
+        var obj = collection.find({}).toArray(function(err, result) {
+          if (err) throw err;
+          result.sort(function (a,b){
+            return  b.wins-a.wins;
+          });
+          console.log(result);
+          
+          var tbl = '<table cellpadding="1" id="table" class="leaderboard">'
+          tbl = tbl +'<tr class="leaderboard" >'
+          +'<th class="leaderboard">Superhero Name</th>'
+          +'  <th class="leaderboard">Wins</th>'
+           +'<tr class="leaderboard">';
+          for(var i = 0; i<result.length; i++)
+                        {
+                            tbl = tbl+ "<td class='leaderboard'>"+result[i].name+"</td>"
+                            + "<td class='leaderboard'>"+result[i].wins+"</td></tr>" 
+                        }
+
+
+            tbl = tbl + "</table> </body></html>"
+          res.send(css+ html +tbl );
+          //client.close();
+      });
+     
+        
       
       
       });
@@ -38,7 +62,7 @@ app.use(express.static('client'));
         
           collection.updateOne({"name":supename, "wins":{$gte:1}}, {$inc : {"wins":1}}, {upsert:true},function(err,rest){
             if (err) throw err;
-            res.send('hopefully it worked?');
+            res.send('updated database');
           });
         });
 
