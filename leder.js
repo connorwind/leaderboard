d151513e42ed8be4279ca55af9aacc9d90e1a637
connorwind <60148037+connorwind@users.noreply.github.com>
2020-04-27 15:04:38 -0400

    Add files via upload

--- leder.js
+++ leder.js
@@ -1,35 +1,48 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://connor:Peppet12@cluster0-lmisd.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {useUnifiedTopology: true} );
+const client = new MongoClient(uri, {useUnifiedTopology: true}, {useCreateIndex: true} );
 const http = require('http');
+var express = require('express');
+var app = express();
 var PORT = process.env.PORT || 5000;
 var results;
+var server = http.Server(app);
+app.use(express.static('client'));
 
-client.connect(err => {
+
+
+
+  client.connect(err=>{
     const collection = client.db("superhero").collection("leaderboard");
-      
-  
-      
+    
       var obj = collection.find({}).toArray(function(err, result) {
           if (err) throw err;
           results = result;
           console.log(results);
-          
-  
-    client.close();
+          client.close();
       });
-  });
+    });
 
 
-const server = http.createServer((request, response) => {
 
-     response.writeHead(200, {'Content-Type': 'application/json'});
-     response.write(JSON.stringify(results));
-     response.end();
+
+
+    server.listen(PORT,function() {
+    console.log("server is running");
+    });
+
+
+    
+
+
+
+app.get('/', function(req,res){
+ 
+  res.send(results)
+
 
 });
 
-server.listen(PORT);
 
 
 
